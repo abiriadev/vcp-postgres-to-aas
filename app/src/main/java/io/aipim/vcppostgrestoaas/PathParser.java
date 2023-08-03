107588f637912d560cb142f1cfdf2f7735ea9df1
Abiria <abiria.dev@gmail.com>
2023-08-03 15:05:02 +0900

    fix: handle several edge cases

--- app/src/main/java/io/aipim/vcppostgrestoaas/PathParser.java
+++ app/src/main/java/io/aipim/vcppostgrestoaas/PathParser.java
@@ -4,13 +4,26 @@ import java.util.Optional;
 
 public class PathParser {
 
-	static String sep = "／";
+	static char sep = '／';
+
+	public static long depth(String path) {
+		var c = path
+			.chars()
+			.filter(ch -> ch == sep)
+			.count();
+		if (c == 0) throw new IllegalArgumentException();
+		return c == 1 && path.equals("" + sep) ? 0 : c;
+	}
 
 	public static Optional<String> parent(String path) {
-		var lio = path.lastIndexOf(sep);
+		var depth = depth(path);
 
-		return lio == 0
+		return depth == 0
 			? Optional.empty()
-			: Optional.of(path.substring(0, lio));
+			: depth == 1
+				? Optional.of("" + sep)
+				: Optional.of(
+					path.substring(0, path.lastIndexOf(sep))
+				);
 	}
 }
