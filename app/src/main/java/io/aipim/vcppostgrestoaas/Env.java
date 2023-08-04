7791e7b0dd435ca6501a08d1dbcc422d6f9b5296
Abiria <abiria.dev@gmail.com>
2023-08-04 17:29:00 +0900

    feat: use semantical names

--- app/src/main/java/io/aipim/vcppostgrestoaas/Env.java
+++ app/src/main/java/io/aipim/vcppostgrestoaas/Env.java
@@ -3,10 +3,13 @@ package io.aipim.vcppostgrestoaas;
 import java.util.List;
 import org.eclipse.digitaltwin.aas4j.v3.model.AssetKind;
 import org.eclipse.digitaltwin.aas4j.v3.model.Environment;
+import org.eclipse.digitaltwin.aas4j.v3.model.KeyTypes;
+import org.eclipse.digitaltwin.aas4j.v3.model.ReferenceTypes;
 import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElement;
 import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultAssetAdministrationShell;
 import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultAssetInformation;
 import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultEnvironment;
+import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultKey;
 import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultReference;
 import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultSpecificAssetID;
 import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultSubmodel;
@@ -19,7 +22,8 @@ public class Env {
 		return new DefaultEnvironment.Builder()
 			.assetAdministrationShells(
 				new DefaultAssetAdministrationShell.Builder()
-					.id("https://aipim.io/DataCatalog")
+					.id("https://aipim.io/DataCatalog/1")
+					.idShort("DataCatalog1")
 					.assetInformation(
 						new DefaultAssetInformation.Builder()
 							.assetKind(AssetKind.INSTANCE)
@@ -30,9 +34,9 @@ public class Env {
 								List.of(
 									new DefaultSpecificAssetID.Builder()
 										.name(
-											"spec astid 1"
+											"specificAssetId"
 										)
-										.value("v1")
+										.value("id1")
 										.build()
 								)
 							)
@@ -40,13 +44,31 @@ public class Env {
 					)
 					.submodels(
 						new DefaultReference.Builder()
-							.keys(List.of())
+							.type(
+								ReferenceTypes.MODEL_REFERENCE
+							)
+							.keys(
+								List.of(
+									new DefaultKey.Builder()
+										.type(
+											KeyTypes.SUBMODEL
+										)
+										.value(
+											"https://aipim.io/DataCatalog/1/ModelEntry"
+										)
+										.build()
+								)
+							)
 							.build()
 					)
 					.build()
 			)
 			.submodels(
 				new DefaultSubmodel.Builder()
+					.id(
+						"https://aipim.io/DataCatalog/1/ModelEntry"
+					)
+					.idShort("ModelEntry")
 					.submodelElements(subel)
 					.build()
 			)
